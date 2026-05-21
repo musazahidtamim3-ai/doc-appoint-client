d8d9a079a1db30b93d034eb631a8b416afafc610
musazahidtamim3-ai <musazahidtamim3@gmail.com>
2026-05-21 18:55:34 +0300

    initial setup for jwt

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
 
 
 let client;
@@ -28,6 +29,16 @@ export const auth = betterAuth({
                clientSecret: process.env.GOOGLE_CLIENT_SECRET,
           },
      },
+     session: {
+          cookieCache: {
+               enabled: true,
+               strategy: "jwt",
+               maxAge:7*24*60*60
+          }
+     },
+     plugins: [
+          jwt()
+     ],
      database: mongodbAdapter(db, {
           // Optional: if you don't provide a client, database transactions won't be enabled.
           client: client

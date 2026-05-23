f900c00d14e8592492049c2589fb1452a2bfbe02
musazahidtamim3-ai <musazahidtamim3@gmail.com>
2026-05-23 06:27:09 +0300

    some problem fixed

--- next.config.mjs
+++ next.config.mjs
@@ -4,16 +4,28 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
-      }
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'png.pngtree.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   reactCompiler: true,
